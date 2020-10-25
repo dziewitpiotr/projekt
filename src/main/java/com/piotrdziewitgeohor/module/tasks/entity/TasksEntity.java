cf3f1e62bff454df7e4c7539a05b8d45d37eeec3
Piotr Dziewit <dziewitpiotr@gmail.com>
2020-10-25 13:01:38 +0100

    projekt koncowy

--- src/main/java/com/piotrdziewitgeohor/module/tasks/entity/TasksEntity.java
+++ src/main/java/com/piotrdziewitgeohor/module/tasks/entity/TasksEntity.java
@@ -68,4 +68,169 @@ public class TasksEntity {
 
     //wskazowki do zlecenia np. godzina, telefon do kogos innego niz wykonawca itp.
     private String taskTip;
-   }
+
+
+    public Long getTaksId() {
+        return taksId;
+    }
+
+    public TasksEntity setTaksId(Long taksId) {
+        this.taksId = taksId;
+        return this;
+    }
+
+    public Long getApplicantId() {
+        return applicantId;
+    }
+
+    public TasksEntity setApplicantId(Long applicantId) {
+        this.applicantId = applicantId;
+        return this;
+    }
+
+    public Long getFieldSurveyorId() {
+        return fieldSurveyorId;
+    }
+
+    public TasksEntity setFieldSurveyorId(Long fieldSurveyorId) {
+        this.fieldSurveyorId = fieldSurveyorId;
+        return this;
+    }
+
+    public Long getOfficeSurveyorId() {
+        return officeSurveyorId;
+    }
+
+    public TasksEntity setOfficeSurveyorId(Long officeSurveyorId) {
+        this.officeSurveyorId = officeSurveyorId;
+        return this;
+    }
+
+    public LocalDateTime getOrderedOn() {
+        return orderedOn;
+    }
+
+    public TasksEntity setOrderedOn(LocalDateTime orderedOn) {
+        this.orderedOn = orderedOn;
+        return this;
+    }
+
+    public LocalDateTime getDateOfDevelopment() {
+        return dateOfDevelopment;
+    }
+
+    public TasksEntity setDateOfDevelopment(LocalDateTime dateOfDevelopment) {
+        this.dateOfDevelopment = dateOfDevelopment;
+        return this;
+    }
+
+    public LocalDateTime getDateOfExecutionInTheField() {
+        return dateOfExecutionInTheField;
+    }
+
+    public TasksEntity setDateOfExecutionInTheField(LocalDateTime dateOfExecutionInTheField) {
+        this.dateOfExecutionInTheField = dateOfExecutionInTheField;
+        return this;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public TasksEntity setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+        return this;
+    }
+
+    public String getTaskObject() {
+        return taskObject;
+    }
+
+    public TasksEntity setTaskObject(String taskObject) {
+        this.taskObject = taskObject;
+        return this;
+    }
+
+    public String getStartMileage() {
+        return startMileage;
+    }
+
+    public TasksEntity setStartMileage(String startMileage) {
+        this.startMileage = startMileage;
+        return this;
+    }
+
+    public String getEndMileage() {
+        return endMileage;
+    }
+
+    public TasksEntity setEndMileage(String endMileage) {
+        this.endMileage = endMileage;
+        return this;
+    }
+
+    public Integer getSection() {
+        return section;
+    }
+
+    public TasksEntity setSection(Integer section) {
+        this.section = section;
+        return this;
+    }
+
+    public AssortmentType getAssortmentType() {
+        return assortmentType;
+    }
+
+    public TasksEntity setAssortmentType(AssortmentType assortmentType) {
+        this.assortmentType = assortmentType;
+        return this;
+    }
+
+    public AssortmentDetails getAssortmentDetails() {
+        return assortmentDetails;
+    }
+
+    public TasksEntity setAssortmentDetails(AssortmentDetails assortmentDetails) {
+        this.assortmentDetails = assortmentDetails;
+        return this;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public TasksEntity setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+        return this;
+    }
+
+    public TaskType getTaskType() {
+        return taskType;
+    }
+
+    public TasksEntity setTaskType(TaskType taskType) {
+        this.taskType = taskType;
+        return this;
+    }
+
+    public boolean isDone() {
+        return IsDone;
+    }
+
+    public TasksEntity setDone(boolean done) {
+        IsDone = done;
+        return this;
+    }
+
+    public String getTaskTip() {
+        return taskTip;
+    }
+
+    public TasksEntity setTaskTip(String taskTip) {
+        this.taskTip = taskTip;
+        return this;
+    }
+}
+
+
--- src/main/java/com/piotrdziewitgeohor/module/tasks/service/TaskService.java
+++ src/main/java/com/piotrdziewitgeohor/module/tasks/service/TaskService.java
@@ -1,5 +1,19 @@
 package com.piotrdziewitgeohor.module.tasks.service;
 
+import com.piotrdziewitgeohor.module.tasks.entity.TasksEntity;
+import com.piotrdziewitgeohor.module.tasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public TasksEntity getOne(Long taskID){
+        return taskRepository.getOne(taskID);
+    }
+
+
 }
